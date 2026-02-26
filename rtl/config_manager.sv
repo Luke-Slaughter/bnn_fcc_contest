01deb12deeac9bb37004e614f804ce94db360fa4
Luke-Slaughter <lukeaslau@gmail.com>
2026-02-26 01:02:32 -0500

    All signals decided in config manager

--- rtl/config_manager.sv
+++ rtl/config_manager.sv
@@ -1,7 +1,8 @@
 /*
 
 Description:
-Basically all RAMS share the same address and the same data line
+- Basically all RAMS share the same address and the same data line
+- Use the config_last signal to demux to the next ram, increment current ram by 1
 
 Inputs:
     input  logic                          config_valid,   //if its valid and should be kept - move counter here
@@ -10,10 +11,13 @@ Inputs:
     input  logic                          config_last,    //Marks the last beat of a packet
 
     input  logic                          clk
+    input logic                           rst,
 Outputs:
     output  logic [ADDR_WIDTH-1:0]        addr;
     output  logic [DATA_WIDTH-1:0]        data_in;
-    output  logic [NUM_RAMS-1:0]          we;
+    output  logic [NUM_RAMS-1:0]          we; 
+
+    output logic                          config_ready; //important to interface with the data in
 
 Internal Signals:
 
@@ -27,10 +31,18 @@ FIFO:
     output              	empty, 				// FIFO is empty when high
                             full 				// FIFO is full when high
 
+Other:
+
+// Address counter for the current RAM to write to the RAM
+//Increments every time a valid configuration word is written.
+// Reset to 0 when move to the next RAM
+    logic [ADDR_WIDTH-1:0] addr_counter;
+
 */
 
 module config_manager #(
-    parameter int NUM_RAMS  = 8
+    parameter int NUM_RAMS  = 8,
+    parameter int NUM_DATA  = 64
 );
 
 
